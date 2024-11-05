89bfddab43249cf380b88045dc5303bbcefdd807
Dario de souza <darioo_dario@hotmail.com>
2024-11-05 18:47:46 -0300

    seed para cadastrar usuário no banco

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -10,6 +10,18 @@ public class DbContexto : DbContext
     _configAppSettings = configAppSettings;
   }
   public DbSet<Administrador> Administradores { get; set; } = default!;
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Administrador>().HasData(
+      new Administrador
+      {
+        Id = 1,
+        Email = "administrador@teste.com",
+        Senha = "123456",
+        Perfil = "Adm"
+      }
+    );
+  }
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
     var stringConexao = _configAppSettings.GetConnectionString("sqlserver");
