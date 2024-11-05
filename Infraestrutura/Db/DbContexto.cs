c2096f0ca95f5f2addde97730de6a5359b142be5
Dario de souza <darioo_dario@hotmail.com>
2024-11-04 22:13:30 -0300

    conexão com o banco de dados realizada

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -4,9 +4,18 @@ using minimal_api.Dominio.Entidades;
 namespace minimal_api.Infraestrutura.Db;
 public class DbContexto : DbContext
 {
+  private readonly IConfiguration _configAppSettings;
+  public DbContexto(IConfiguration configAppSettings)
+  {
+    _configAppSettings = configAppSettings;
+  }
   public DbSet<Administrador> Administradores { get; set; } = default!;
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    optionsBuilder.UseMySql("string conexão", ServerVersion.AutoDetect("string conexão"));
+    var stringConexao = _configAppSettings.GetConnectionString("sqlserver");
+    if (!string.IsNullOrEmpty(stringConexao))
+    {
+      optionsBuilder.UseSqlServer(stringConexao);
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("sqlserver"));
+});
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/login", (LoginDTO loginDTO) =>
