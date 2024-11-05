b7c7a88168424ed5964ca7bb95a2a126abc2ce29
Dario de souza <darioo_dario@hotmail.com>
2024-11-04 21:28:27 -0300

    config banco com sql server

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,5 +1,12 @@
-namespace minimal_api.Infraestrutura.Db;
-public class DbCOntexto
-{
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
+namespace minimal_api.Infraestrutura.Db;
+public class DbContexto : DbContext
+{
+  public DbSet<Administrador> Administradores { get; set; } = default!;
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseMySql("string conexão", ServerVersion.AutoDetect("string conexão"));
+  }
 }
